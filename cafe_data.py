25e3300b12215cb6cd72892f31bc876ba121b961
properchloe <cmonson.f21@warren-wilson.edu>
2021-12-12 20:38:20 -0500

    putting csv data into the tables

--- cafe_data.py
+++ cafe_data.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 
 def create_database():
@@ -39,3 +40,57 @@ def create_database():
         s3.execute("CREATE  INDEX stores_store_name_index ON stores(store_name)")
         s3.execute("CREATE  INDEX ratings_date_index ON ratings(date)")
         s3.execute("CREATE  INDEX specials_date_index ON specials(date)")
+
+
+def drop_tables():
+    with sqlite3.connect('data/cafe.sqlite3') as s3:
+        s3.execute("DROP TABLE if exists coffees")
+        s3.execute("DROP TABLE if exists specials")
+        s3.execute("DROP TABLE if exists ratings")
+        s3.execute("DROP TABLE if exists stores")
+
+
+def load_cafe_data():
+    drop_tables()
+    create_database()
+    with sqlite3.connect('data/cafe.sqlite3') as s3:
+        with open("coffees.csv", 'r') as coffees_csv:
+            done = False
+            while not done:
+                this_reader = csv.DictReader(coffees_csv, delimiter="\t")
+                try:
+                    thing = next(this_reader)
+                    s3.execute('INSERT INTO coffees (coffee_name, type, cost_lb) VALUES (:coffee_name, :type, :cost_lb)', thing)
+                except StopIteration:
+                    done = True
+        with open("ratings.csv", 'r') as ratings_csv:
+            done = False
+            while not done:
+                this_reader = csv.DictReader(ratings_csv, delimiter="\t")
+                try:
+                    thing = next(this_reader)
+                    s3.execute('INSERT INTO ratings (date, store_name, rating) VALUES (:date, :store_name, :rating)', thing)
+                except StopIteration:
+                    done = True
+        with open("specials.csv", 'r') as specials_csv:
+            done = False
+            while not done:
+                this_reader = csv.DictReader(specials_csv, delimiter="\t")
+                try:
+                    thing = next(this_reader)
+                    s3.execute('INSERT INTO specials (date, store_name, coffee_name, price) VALUES (:date, :store_name, :coffee_name, :price)', thing)
+                except StopIteration:
+                    done = True
+        with open("stores.csv", 'r') as stores_csv:
+            done = False
+            while not done:
+                this_reader = csv.DictReader(stores_csv, delimiter="\t")
+                try:
+                    thing = next(this_reader)
+                    s3.execute('INSERT INTO stores (store_name, type, cost_lb) VALUES (:store_name, :type, :cost_lb)', thing)
+                except StopIteration:
+                    done = True
+
+
+if __name__ == "__main__":
+    load_cafe_data()
